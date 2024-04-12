aa87413adfcceaa0c8de1fbe1d9b6755f8dad506
sillah0 <siladennis624@gmail.com>
2024-04-12 23:53:19 +0300

    fix indentation

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,30 +3,28 @@
 /**
  * binary_tree_insert_left - Inserts a node as the left-child of another node.
  *
- * @parent: Pointer to the node to insert the left-child in.
- * @value: The value to store in the new node.
+ * @parent: A pointer to the node to insert the left child in.
+ * @value: The value to store the new node.
  *
- * Return: A pointer to the created node, NULL on failure or if parent is NULL
+ * Return: A pointer to the created node, NULL on failure.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new;
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = parent->left;
-	new_node->right = NULL;
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
 
-	if (parent->left)
-		parent->left->parent = new_node;
-	parent->left = new_node;
-
-	return (new_node);
+	return (new);
 }
